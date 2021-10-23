88a40710be3225aed4edcfd4c825b58f0b1e8f3d
Дмитрий Никифоров <dnikiforov@innoseti.ru>
2021-10-23 19:16:11 +0200

    feat: added miniapps bridge

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,16 @@ import "./index.css";
 import { App } from "./App";
 import { AdaptivityProvider, ConfigProvider } from "@vkontakte/vkui";
 import "@vkontakte/vkui/dist/vkui.css";
+import bridge from "@vkontakte/vk-bridge";
+
+bridge.send("VKWebAppInit", {});
+
+// bridge.subscribe((e) => {
+//   console.log("bridge event", e);
+// });
 
 ReactDOM.render(
-  <ConfigProvider>
+  <ConfigProvider scheme="inherit">
     <AdaptivityProvider>
       <App />
     </AdaptivityProvider>
